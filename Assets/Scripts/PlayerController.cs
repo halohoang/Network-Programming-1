19c129c9366567370dffa1c7ec668313e4b09f6a
Hayoon <hayoonee@gmail.com>
2021-01-14 16:42:14 +0900

    added playercontroller scripts &groundcheck
    and deleted demo folders of photon

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject cameraHolder;
+    [SerializeField] float mouseSensitivity, runSpeed, walkSpeed, jumpForce, smoothTime;
+
+    private float verticalLookRotation;
+    private bool grounded;
+    Vector3 smoothMoveVelocity;
+    Vector3 moveAmount;
+
+    Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Look();
+        Move();
+        Jump();
+        
+    }
+
+    void Move()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed),
+            ref smoothMoveVelocity, smoothTime);
+    }
+
+    void Look()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+        verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+    }
+
+    public void SetGroundedState (bool _grounded)
+    {
+        grounded = _grounded;
+    }
+}
---
+++ Assets/Scripts/PlayerGroundCheck.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCheck : MonoBehaviour
+{
+    PlayerController PlayerController;
+
+
+    void Awake()
+    {
+        PlayerController = GetComponentInParent<PlayerController>();
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == PlayerController.gameObject)
+            return;
+
+        PlayerController.SetGroundedState(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == PlayerController.gameObject)
+            return;
+
+        PlayerController.SetGroundedState(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        PlayerController.SetGroundedState(true);
+    }
+}
+
