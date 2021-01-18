68b39d2560a22a3ef50fbf48bd94b6ff158d9543
Hayoon <hayoonee@gmail.com>
2021-01-18 19:40:20 +0900

    player script for online networking added
    worked on player online settings

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+
+public class PlayerScript : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public Rigidbody2D RB;
+    public Animator AN;
+    public SpriteRenderer SR;
+    public PhotonView PV;
+    public Text NickNameText;
+    public Image HealthImage;
+
+    bool isGround;
+    Vector3 curPos;
+
+
+
+    void Awake()
+    {
+        // Nickname
+        NickNameText.text = PV.IsMine ? PhotonNetwork.NickName : PV.Owner.NickName;
+        NickNameText.color = PV.IsMine ? Color.green : Color.red;
+
+     
+    }
+
+
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            //movement
+            float axis = Input.GetAxisRaw("Horizontal");
+            RB.velocity = new Vector2(4 * axis, RB.velocity.y);
+
+            if (axis != 0)
+            {
+                AN.SetBool("walk", true);
+                PV.RPC("FlipXRPC", RpcTarget.AllBuffered, axis); //for the reconnect, activate flipX
+            }
+            else AN.SetBool("walk", false);
+
+
+            // ↑ Jump, groundcheck
+            isGround = Physics2D.OverlapCircle((Vector2)transform.position + new Vector2(0, -0.5f), 0.07f, 1 << LayerMask.NameToLayer("Ground"));
+            AN.SetBool("jump", !isGround);
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isGround) PV.RPC("JumpRPC", RpcTarget.All);
+
+
+        
+        }
+        
+        else if ((transform.position - curPos).sqrMagnitude >= 100) transform.position = curPos;
+        else transform.position = Vector3.Lerp(transform.position, curPos, Time.deltaTime * 10);
+    }
+
+
+    [PunRPC]
+    void FlipXRPC(float axis) => SR.flipX = axis == -1;
+
+    [PunRPC]
+    void JumpRPC()
+    {
+        RB.velocity = Vector2.zero;
+        RB.AddForce(Vector2.up * 700);
+    }
+
+    public void Hit()
+    {
+        HealthImage.fillAmount -= 0.1f;
+        if (HealthImage.fillAmount <= 0)
+        {
+            GameObject.Find("Canvas").transform.Find("RespawnPanel").gameObject.SetActive(true);
+            PV.RPC("DestroyRPC", RpcTarget.AllBuffered); 
+        }
+    }
+
+    [PunRPC]
+    void DestroyRPC() => Destroy(gameObject);
+
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(HealthImage.fillAmount);
+        }
+        else
+        {
+            curPos = (Vector3)stream.ReceiveNext();
+            HealthImage.fillAmount = (float)stream.ReceiveNext();
+        }
+    }
+}
+
