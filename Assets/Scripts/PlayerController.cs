307704d0ff7f32be54f904fd3e2c8e3da0fc4dbf
Hayoon <hayoonee@gmail.com>
2021-01-19 17:43:13 +0900

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,55 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPun, IPunObservable
 {
-    public float moveSpeed = 5f;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] new Rigidbody2D rigidbody;
+    [SerializeField] TrailRenderer trailRenderer;
+    
 
-    public Rigidbody2D rb;
-    public Camera cam;
-    public Animator anim;
+    [SerializeField] UnityEngine.UI.Text nicknameText;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector2 groundedCheckSize;
+    
+    private float horizontal;
+    private float vertical;
+    
 
-    Vector2 movement;
-    Vector2 mousePos;
-
-    private void Update()
+    private void Start()
     {
-        ProcessInputs();
-
-        anim.SetFloat("Horizontal", movement.x);
-        anim.SetFloat("Vertical", movement.y);
-        anim.SetFloat("Speed", movement.sqrMagnitude);
-
-
-
-
-    }
-    void FixedUpdate()
-    {
-        Move();
-        Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x)*Mathf.Rad2Deg-90f;
-        rb.rotation = angle;
-    }
-
-
-    void ProcessInputs()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        nicknameText.text = photonView.Owner.NickName;
+        SetLocalColors();
         
     }
 
-    void Move()
+    private void SetLocalColors()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-       
+        Color color = GetColorForPlayerById(photonView.OwnerActorNr);
+        nicknameText.color = color;
 
+        trailRenderer.startColor = color;
+        color.a = 0;
+        trailRenderer.endColor = color;
     }
 
-    
+    /*private Color GetColorForPlayerById(int id)
+    {
+        return Color.HSVToRGB((float)id / 10f, 1, 1);
+    }*/
+
+    private void FixedUpdate()
+    {
+        if (photonView.IsMine)
+        {
+            var oldVertical = vertical;
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+
+            
+            rigidbody.velocity = new Vector2(horizontal * moveSpeed, rigidbody.velocity.y);
+
+        }
+
+      
+    }
+
+   
+
+    [PunRPC]
+    private bool IsGrounded()
+    {
+        var hits = Physics2D.BoxCastAll(transform.position, groundedCheckSize, 0, Vector2.zero, 0);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject != gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, groundedCheckSize);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(horizontal);
+            stream.SendNext(vertical);
+        }
+        else
+        {
+            horizontal = (float)stream.ReceiveNext();
+            vertical = (float)stream.ReceiveNext();
+  
+        }
+    }
 }
